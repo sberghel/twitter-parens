81ec05f9346ca687e07621609fa5822c563161a9
Skye Berghel <sberghel@cs.hmc.edu>
2013-01-14 12:01:51 -0800

    now tracks the number of parens missing

--- parens.pl
+++ parens.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use 5.016;
+use List::Member;
 
 sub matchParens {
     # Who thought this was a clever way to name arguments???
@@ -18,15 +19,52 @@ sub matchParens {
 
     while (my ($index, $elem) = each @chars) {
         if ($elem eq "(") {
-            if ($index == 0) {
-                my $nextchar = $chars[$index+1];
-            }
+            my $nextchar = $chars[$index+1];
             my $prevchar = $chars[$index-1];
+            if ($index == 0) {
+                # Don't look at the previous character, since we're at
+                # the beginning of the string
+                
+                # "Unless" is how perl says "if ![condition]."
+                # member returns -1 if a thing is not a member and 0
+                # if it is.
+                unless ( member($nextchar, @smiley_chars) + 1) {
+                    $unmatched_parens++;
+                }
+            } elsif ($index == $#chars) {
+                # Don't look at the next character, since we're at the
+                # end of the string
+                unless ( member($prevchar, @smiley_chars) + 1) {
+                    $unmatched_parens++;
+                }
+            } else {
+                unless ( (member($prevchar, @smiley_chars) + 1) ||
+                    (member($nextchar, @smiley_chars) + 1)) {
+                    $unmatched_parens++;
+                }
+            }
             say "Look, I found an open paren.";
         } elsif ($elem eq ")") {
-            say "Look, I found a closing paren at index $index";
+            my $nextchar = $chars[$index+1];
+            my $prevchar = $chars[$index-1];
+            if ($index == 0) {
+                unless ( member($nextchar, @smiley_chars) + 1) {
+                    $unmatched_parens--;
+                }
+            } elsif ($index == $#chars) {
+                unless ( member($prevchar, @smiley_chars) + 1) {
+                    $unmatched_parens--;
+                }
+            } else {
+                unless ( (member($prevchar, @smiley_chars) + 1) ||
+                    (member($nextchar, @smiley_chars) +1)) {
+                    $unmatched_parens--;
+                }
+            }
+            say "Look, I found a closing paren";
         }
     }
+    say "I found $unmatched_parens unmatched parens";
 }
 
-matchParens('(2 open (1 close)')
+matchParens('(2 open (1 :) ): close)')
