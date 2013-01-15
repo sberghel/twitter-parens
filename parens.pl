9f4aa72bd231930dbf68a57a5443251c8057e78b
Skye Berghel <sberghel@cs.hmc.edu>
2013-01-14 18:06:53 -0800

    The paren machine is done.

--- parens.pl
+++ parens.pl
@@ -9,16 +9,21 @@ use Net::Twitter;
 use Path::Class;
 use Try::Tiny;
 
-our @TRAITS = ['API::REST', 'OAuth'];
+our @TRAITS = ['API::REST', 'OAuth', 'AutoCursor'];
 # magical Twitter magic
 our %MAGIC = (pbafhzre_xrl => 'HInWH4kPhPjPuM8ZMB6D',
     pbafhzre_frperg => 'ygHyiL3U6ju7Qw8UdRgVUjA4K2W2NoWvNybdzHx');
 our $TOKEN_FILE = "parens.oauth";
 our $TOKEN_DIR = "./";
+our $TWEETS_FILE = "last.tweet";
 our $NT;
 our $USERNAME;
+# Debug variable. If it evaluates to True, you'll get debug messages on
+# STDERR.
+# If it evaluates to False, you won't.
+our $DEBUG = 1;
 
-sub findParens {
+sub find_parens {
     # Who thought this was a clever way to name arguments???
     my ($text) = @_;
 
@@ -54,12 +59,11 @@ sub findParens {
                     $unmatched_parens++;
                 }
             }
-        } elsif ($elem eq ")") {
+        } elsif ( ($elem eq ")") && ($unmatched_parens > 0) ) {
             my $nextchar = $chars[$index+1];
             my $prevchar = $chars[$index-1];
             if ($index == 0) {
                 unless ( member($nextchar, @smiley_chars) + 1) {
-                    $unmatched_parens--;
                 }
             } elsif ($index == $#chars) {
                 unless ( member($prevchar, @smiley_chars) + 1) {
@@ -76,17 +80,17 @@ sub findParens {
     return $unmatched_parens;
 }
 
-sub matchParens {
-    my ($parensToMatch) = @_;
+sub match_parens {
+    my ($parens_to_match) = @_;
     my $paren_string = "";
-    for (my $i = 0; $i < $parensToMatch; $i++) {
+    for (my $i = 0; $i < $parens_to_match; $i++) {
         $paren_string .= ")";
     }
     return $paren_string;
 }
 
-my $parens = findParens('(2 open (1 :) ): :( ;( ;) ); close)');
-my $string = matchParens($parens);
+my $parens = find_parens('(2 open (1 :) ): :( ;( ;) ); close)');
+my $string = match_parens($parens);
 
 sub log_into_twitter {
     # We need a temporary variable for this, otherwise Net::Twitter->new
@@ -98,9 +102,8 @@ sub log_into_twitter {
     );
 
     my ($username, $access_token, $access_token_secret) = restore_tokens();
-    say STDERR "username is $username";
+    $USERNAME = $username;
     if ($access_token && $access_token_secret) {
-        say STDERR "we are logged in!";
         $NT->access_token($access_token);
         $NT->access_token_secret($access_token_secret);
     }
@@ -111,54 +114,126 @@ sub log_into_twitter {
         say " and enter the authorization pin";
 
         my $pin = <STDIN>;
-        say STDERR "got the pin";
         chomp $pin;
-        say STDERR "chomped the pin";
 
         my ($access_token, $access_token_secret, $user_id,
             $screen_name) = $NT->request_access_token(verifier => $pin);
-        say STDERR "got tokens";
+        $USERNAME = $screen_name;
         save_tokens($access_token, $access_token_secret);
-        say STDERR "tokens saved";
     }
-    $USERNAME = $username;
-}
 
-sub make_a_tweet {
-    $NT->update({status => "A valiant attempt at a status update"});
+    if ($DEBUG) {
+        say STDERR "Logged in as ", $USERNAME;
+    }
 }
 
 sub get_timeline_tweets {
     my $tweets = $NT->home_timeline();
-    say "Here are some tweets:";
-    say Dumper($tweets);
+    if ($DEBUG) {
+        say STDERR "Here are some tweets:";
+        foreach my $tweet (@{ $tweets }) {
+            say STDERR $tweet->{text};
+        }
+        return @{ $tweets };
+    }
+}
+
+sub find_parens_in_tweets {
+    my @tweets = @_;
+    my $last_tweet = get_last_replied_tweet();
+    if ($DEBUG) {
+        say STDERR "We last replied to ", $last_tweet;
+    }
+    foreach my $tweet (@tweets) {
+        my $num_parens = find_parens($tweet->{text});
+        if ($DEBUG) {
+            say "We found $num_parens unmatched parens";
+        }
+        if ($num_parens > 0) {
+            if ($tweet->{id} > $last_tweet) {
+                my $new_tweet = match_parens($num_parens);
+                $new_tweet = "@" . $tweet->{user}->{screen_name} . 
+                    " " . $new_tweet;
+                if ($DEBUG) {
+                    say STDERR "We are going to tweet: $new_tweet";
+                    say STDERR "And we will tweet it in reply to ", 
+                        $tweet->{id},
+                        " by the user ", $tweet->{user}->{screen_name};
+                }
+
+                # We have to save in this function.
+                # Otherwise if replying crashes, we never save.
+                # (Sometimes "update" throws an error when it succeeds.)
+                save_last_replied_tweet($tweet->{id});
+
+                # Reply to that tweet
+                $NT->update({status => $new_tweet,
+                        in_reply_to_status_id => $tweet->{id} });
+                if ($DEBUG) {
+                    say STDERR "replied";
+                }
+            } else {
+                if ($DEBUG) {
+                    say STDERR "we already responded to ", $tweet->{id};
+                }
+            }
+        }
+    }
+}
+
+sub save_last_replied_tweet {
+    my ($tweet_id) = @_;
+    my $file = dir($TOKEN_DIR)->file($TWEETS_FILE);
+    $file->spew($tweet_id);
+}
+
+sub get_last_replied_tweet {
+    try {
+        my $tweet = get_tweet_from_file();
+        if ($DEBUG) {
+            say STDERR "got tweet from file";
+        }
+        return $tweet;
+    } catch {
+        say STDERR "failed to get tweet from file; defaulting to 0";
+        return 0;
+    }
+}
+
+sub get_tweet_from_file {
+    my $file = dir($TOKEN_DIR)->file($TWEETS_FILE);
+    my @lines = $file->slurp();
+    if ($#lines == -1) {
+        #if the file is empty, assume we've never responded
+        return 0;
+    } elsif ($#lines == 0) {
+        #if we have just one line, return that one
+        return $lines[0];
+    } else {
+        die "We can't have more than one last tweet";
+    }
 }
 
 sub restore_tokens {
     try {
         my @tokens = get_tokens_from_file();
-        say STDERR "got the tokens @tokens from the file";
         return @tokens;
     } catch {
-        say STDERR "unable to read tokens from file";
+        say STDERR "Unable to read tokens from file.";
         return ("", "");
     }
 }
 
 sub get_tokens_from_file {
-    my $dir = dir($TOKEN_DIR); 
-    my $file = $dir->file($TOKEN_FILE);
+    my $file = dir($TOKEN_DIR)->file($TOKEN_FILE); 
     my $file_handle = $file->openr();
-    say STDERR "file open now for getting tokens";
 
     my $content = $file->slurp();
     my @tokens = ();
     while (my $line = $file_handle->getline()) {
         chomp($line);
-        say STDERR "line was $line";
         push(@tokens, $line);
     }
-    say STDERR "tokens are ", Dumper(@tokens);
     return @tokens;
 }
 
@@ -167,12 +242,68 @@ sub save_tokens {
     my $dir = dir($TOKEN_DIR);
     my $file = $dir->file($TOKEN_FILE);
     my $file_handle = $file->openw();
-    say STDERR "file open for saving tokens";
     foreach my $token (@tokens) {
-        say STDERR "saving the token $token now";
         $file_handle->print($token . "\n");
     }
 }
 
+sub get_followers {
+    my $followers_ref = $NT->followers_ids( {"screen_name" => $USERNAME} );
+    return ref_to_array($followers_ref);
+}
+
+sub ref_to_array {
+    my ($ref) = @_;
+    my @array;
+    # The ref could be a ref either an array or a hash.
+    # Make sure we set the array to a list of IDs.
+    if (ref($ref) eq "HASH") {
+        @array = $ref->{ids};
+    } else {
+        @array = @{ $ref };
+    }
+    return @array;
+}
+
+sub get_friends {
+    my $friends_ref = $NT->friends_ids( {"screen_name" => $USERNAME} );
+    return ref_to_array($friends_ref);
+}
+
+sub prune_friends {
+    my @followers = get_followers();
+    my @friends = get_friends();
+    for my $friend (@friends) {
+        unless ( member($friend, @followers) + 1){
+            if ($DEBUG) {
+                print STDERR "unfollowing user with id $friend";
+                print STDERR " and screenname ";
+                my $user = $NT->lookup_users({"user_id" => $friend});
+                say STDERR @{$user}[0]->{screen_name};
+            }
+            $NT->destroy_friend({"user_id" => $friend});
+        }
+    }
+}
+
+sub add_friends {
+    my @followers = get_followers();
+    my @friends = get_friends();
+    for my $follower (@followers) {
+        unless ( member($follower, @friends) + 1) {
+            if ($DEBUG) {
+                print STDERR "following user with id $follower";
+                print STDERR " and screenname ";
+                my $user = $NT->lookup_users({"user_id" => $follower});
+                say STDERR @{$user}[0]->{screen_name};
+            }
+            $NT->create_friend({"user_id" => $follower});
+        }
+    }
+}
+
 log_into_twitter();
-get_timeline_tweets();
+prune_friends();
+add_friends();
+#my @tweets = get_timeline_tweets();
+#find_parens_in_tweets(@tweets);
